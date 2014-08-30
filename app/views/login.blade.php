09c6b6f4c5918e9392444005e1d4764a654e3154
David <david@weberish.com>
2014-08-30 15:19:06 -0700

    Adding Bootstrap and jQuery files, creating initial views

---
+++ app/views/login.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title>TODO</title>
+	<link type="text/css" rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
+	<link type="text/css" rel="stylesheet" href="{{ asset('css/bootstrap-theme.min.css') }}" />
+    <script src="{{ asset('js/jquery-1.11.1.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+</head>
+<body>
+
+
+</body>
+</html>
