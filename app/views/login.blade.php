89f86fb0d05b1c0eb88160efefe5f9bbbed5082e
David <david@weberish.com>
2014-08-30 17:39:55 -0700

    Creating the login page, various graphics and styling additions

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -6,11 +6,58 @@
 	<title>TODO</title>
 	<link type="text/css" rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
 	<link type="text/css" rel="stylesheet" href="{{ asset('css/bootstrap-theme.min.css') }}" />
+	<link type="text/css" rel="stylesheet" href="{{ asset('css/main.css') }}" />
     <script src="{{ asset('js/jquery-1.11.1.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 </head>
 <body>
+	<div id="mainwrap">
+		<div id="logo"><img src="{{ asset('img/logo_large.png') }}" /></div>
 
-
+		<div class="container-fluid login-container">
+			<div id="tab-wrap">
+			<ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
+				<li class="active"><a href="#login" data-toggle="tab">Login</a></li>
+				<li><a href="#register" data-toggle="tab">Register</a></li>
+			</ul>
+			</div>
+			<div class="tab-content">
+				<div class="tab-pane active" id="login">
+					<div class="row">
+						<div class="col-md-3">Email</div>
+						<div class="col-md-9"><input type="text" name="email" class="login-input" /></div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">Password</div>
+						<div class="col-md-9"><input type="password" name="pass" class="login-input" /></div>
+					</div>
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3"><input type="submit" class="login-submit" value="LOGIN" /></div>
+					</div>
+				</div>
+				<div class="tab-pane" id="register">
+					<div class="row">
+						<div class="col-md-3">Name</div>
+						<div class="col-md-9"><input type="text" name="s-name" class="login-input" /></div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">Email</div>
+						<div class="col-md-9"><input type="text" name="s-email" class="login-input" /></div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">Password</div>
+						<div class="col-md-9"><input type="password" name="s-pass1" class="login-input" /></div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">Confirm Password</div>
+						<div class="col-md-9"><input type="password" name="s-pass2" class="login-input" /></div>
+					</div>
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3"><input type="submit" class="login-submit" value="REGISTER" /></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
--- readme.md
+++ readme.md
@@ -15,7 +15,8 @@ To setup the application, follow the following steps:
 1. Checkout git source to the appropriate server directory
 2. Run "composer update" within root folder to install all vendor code (Composer must be installed for this, see guides elsewhere)
 3. Create a MySQL database, and set up a user with permissions on this db
-4. Move "app/config/sample.database.php" to "app/confing/database.php", then update with appropriate MySQL credentials
+4. Rename "app/config/sample.database.php" to "app/confing/database.php", then update with appropriate MySQL credentials
 5. Run "php artisan migrate" from the root folder to create the database structure
 6. Update "app/config/app.php" with appropriate variables
 7. Set up a symbolic link from your server's publically accessible web directory, pointed to this project's "public" directory
+8. Set permissions on the app/storage directory (recursive) to allow read-write access for the web server
